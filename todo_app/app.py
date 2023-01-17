8bc28cda782ec73179b918013cb4e5c11698c315
Hernando Padron Adriana (ETAS-DEV/XPC-UK) <hoa1yok@bosch.com>
2023-01-17 00:07:03 +0000

    add /action route that gets an item from the form and adds it to the item list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,13 @@
+from urllib import request
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect, url_for
+from flask import session
 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items
+from todo_app.data.session_items import add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -12,3 +17,9 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template('index.html', items=items)
+
+@app.route('/action', methods=['POST', 'GET'])
+def action():
+    new_item = request.form.get('item')
+    items = add_item(new_item)
+    return redirect(url_for(".index"))
