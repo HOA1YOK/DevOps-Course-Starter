6be2f7f6fc6985b17624fd5e65be5d285e892f57
Hernando Padron Adriana (ETAS-DEV/XPC-UK) <hoa1yok@bosch.com>
2023-03-06 16:58:38 +0000

    retrieve complete_item id from index.html and add complete_item function (not completed)

--- todo_app/app.py
+++ todo_app/app.py
@@ -10,11 +10,20 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     items = get_items()
-    return render_template('index.html', items=items)
+    return render_template('index.html', items=items, complete_item=complete_item)
 
 # Create new items
 @app.post('/action')
 def action():
     new_item = request.form.get('item')
     add_card(new_item)
+    return redirect(url_for(".index"))
+
+#Change item status
+@app.post('/complete-item')
+def  complete_item():
+    complete_item = request.form.get('complete_item')
+    #TODO: call update_card_status(this_item)
+    print('**************************')
+    print(complete_item)
     return redirect(url_for(".index"))
