376edc0159bca492727bbd6b3f6a995fa7ff7582
Hernando Padron Adriana (ETAS-DEV/XPC-UK) <hoa1yok@bosch.com>
2023-07-19 14:44:03 +0100

    write a test for each property in ViewModel class for TDD

--- todo_app/data/test_view_model.py
+++ todo_app/data/test_view_model.py
@@ -1,12 +1,31 @@
 """Unit tests for view_model.py """
 
 import pytest
-from todo_app.data.view_model import ViewModel
+
 from todo_app.data.trello_items import Item
+from todo_app.data.view_model import ViewModel
+
+_TEST_ITEMS = [
+    Item(100000, "test_item", "Doing"),
+    Item(200000, "test_item", "Done"),
+    Item(300000, "test_item", "To Do"),
+    Item(400000, "test_item", "Done"),
+]
+
 
 def test_view_model_done_property():
-    test_item = Item(123456789, "test_item", "Done")
-    view_model = ViewModel(test_item).done_items
-    assert(view_model.items.status == "Done")
-   
+    view_model = ViewModel(_TEST_ITEMS).done_items
+    for i in view_model:
+        assert i.status == "Done"
 
+
+def test_view_model_todo_property():
+    view_model = ViewModel(_TEST_ITEMS).todo_items
+    for i in view_model:
+        assert i.status == "To Do"
+
+
+def test_view_model_doing_property():
+    view_model = ViewModel(_TEST_ITEMS).doing_items
+    for i in view_model:
+        assert i.status == "Doing"
