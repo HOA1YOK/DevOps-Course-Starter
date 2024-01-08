0ca8c7a0d2541c037722ae10f3beda9576d4c4a0
Hernando Padron Adriana (ETAS-DEV/XPC-UK) <hoa1yok@bosch.com>
2024-01-08 18:47:58 +0000

    remove unneeded flask Confing class

--- todo_app/app.py
+++ todo_app/app.py
@@ -7,7 +7,6 @@ from todo_app.flask_config import Config
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config())
     trello = TrelloService()
 
     # Display list of items
